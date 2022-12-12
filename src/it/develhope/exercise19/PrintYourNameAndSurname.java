d5b41e6736f3f4b43aa8e6ca68fc61fc9b8c6f75
Jason Di Bartolo <jasondibartolo98@gmail.com>
2022-12-13 00:12:07 +0100

    Initial Commit

---
+++ src/it/develhope/exercise19/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package it.develhope.exercise19;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String name = "Jason", surname = "Di Bartolo";
+        System.out.println(name + "\n" + surname);
+    }
+}
